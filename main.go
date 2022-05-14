8aace9cbf704eb380598cb21bd455e82a1981030
takokun778 <takokun.778@gmail.com>
2022-05-15 08:16:58 +0900

    first commit

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ func main() {
 		log.Fatalln(err.Error())
 	}
 
+	defer f.Close()
+
 	dsn := "postgres://postgres:@localhost:15432/postgres?sslmode=disable"
 	// dsn := "unix://user:pass@dbname/var/run/postgresql/.s.PGSQL.5432"
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
@@ -27,6 +29,8 @@ func main() {
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
+
+	defer conn.Close()
 
 	_, err = pgdriver.CopyTo(context.Background(), conn, f, "COPY sample TO STDOUT DELIMITER ',' CSV HEADER")
 	if err != nil {
